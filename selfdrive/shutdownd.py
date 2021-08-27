9503ca14a6af783a3088c901e996d8ca6d6f3ff1
parksunkyu81 <parksunkyu81@gmail.com>
2021-08-27 18:15:51 +0900

    30 -> 300 초 자동 종료

--- selfdrive/shutdownd.py
+++ selfdrive/shutdownd.py
@@ -6,8 +6,8 @@ import cereal.messaging as messaging
 
 
 def main():
-  # 30초가 지나면 자동 종료된다.
-  shutdown_at = 3 * 10
+  # 300초가 지나면 자동 종료된다.
+  shutdown_at = 3 * 100
   shutdown_count = 0
   device_state_sock = messaging.sub_sock('deviceState')
 
