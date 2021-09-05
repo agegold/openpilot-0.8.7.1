9f2043f24cc49d9ea3c519f8a48d98c2e5dda818
parksunkyu81 <parksunkyu81@gmail.com>
2021-09-05 18:23:14 +0900

    NDA 작업중

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -259,8 +259,6 @@ class Controls:
       else:
         max_speed_clu = self.kph_to_clu(self.v_cruise_kph)
 
-      print("curve_speed_ms : ========================== ", self.curve_speed_ms)
-      print("max_speed_clu : ========================== ", max_speed_clu)
 
       # max_speed_log = "{:.1f}/{:.1f}/{:.1f}".format(float(limit_speed),
       #                                              float(self.curve_speed_ms*self.speed_conv_to_clu),
