85444bdc879c0c4458649a2b5d4a6f834f346622
parksunkyu81 <parksunkyu81@gmail.com>
2021-09-10 13:50:08 +0900

    NDA 조정

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -83,10 +83,6 @@ class CarState(CarStateBase):
 
     return ret
 
-  # 2 lines for 3 Bar distance
-  def get_follow_level(self):
-    return self.follow_level
-
   @staticmethod
   def get_can_parser(CP):
     # this function generates lists for signal, messages and initial values
